0101df1fe314525cd93754bd6dfc2765fb971e92
arfa <arfa@example.com>
2020-02-21 19:12:26 +0700

    http post

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const express = require("express");
+const fs = require('fs');
+const express = require('express');
 
 const app = express();
 
@@ -19,9 +19,9 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: "success",
+    status: 'success',
     results: tours.length,
     data: {
       tours
@@ -29,9 +29,24 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
-app.post("/api/v1/tours", (req, res) => {
-  console.log(req.body);
-  res.send("Done");
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  //   console.log(newId);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const port = 3000;
