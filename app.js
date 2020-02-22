04c6bcba98ab5adc7e4236db2d583673cd574e8f
Ade Rizky <aderizkyfaqi@gmail.com>
2020-02-22 19:13:02 +0700

    responding to url params

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
@@ -19,9 +19,9 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
-    status: 'success',
+    status: "success",
     results: tours.length,
     data: {
       tours
@@ -29,7 +29,28 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  // console.log(id);
+  const tour = tours.find(el => el.id === id);
+
+  // if (id > tours.length) {
+    if (!tour){
+    return res.status(404).json({
+      status: "fail",
+      massage: "Invalid ID"
+    });
+  }
+  
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour
+    }
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -40,7 +61,7 @@ app.post('/api/v1/tours', (req, res) => {
     JSON.stringify(tours),
     err => {
       res.status(201).json({
-        status: 'success',
+        status: "success",
         data: {
           tour: newTour
         }
