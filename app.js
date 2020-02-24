9dc9a5f6b8bc2fc7553fb113bf101478e8b7f82e
arfa <arfa@example.com>
2020-02-24 18:20:04 +0700

    using 3rd-party middleware

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-//middleware
+// 1) Middleware
+// 3rd-party Middleware
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -19,6 +22,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//2) Routs Handler
 const getAllTour = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -105,23 +109,25 @@ const deleteTour = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours', getAllTour);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', postTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
+// app.get('/api/v1/tours', getAllTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', postTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
-// app
-//   .route('/api/v1/tours')
-//   .get(getAllTour)
-//   .post(postTour);
+//3) Routs
+app
+  .route('/api/v1/tours')
+  .get(getAllTour)
+  .post(postTour);
 
-// app
-//   .route('/api/v1/tours/:id')
-//   .get(getTour)
-//   .patch(updateTour)
-//   .delete(deleteTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
+// 4) Start Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is Running on ${port}..`);
