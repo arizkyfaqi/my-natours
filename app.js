51298cb88808729de9ea27c09384582d58710783
arfa <arfa@example.com>
2020-02-25 15:24:51 +0700

    Implementing  the users routs

--- app.js
+++ app.js
@@ -109,6 +109,41 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    massage: 'This rout is not yet defined!'
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    massage: 'This rout is not yet defined!'
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    massage: 'This rout is not yet defined!'
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    massage: 'This rout is not yet defined!'
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    massage: 'This rout is not yet defined!'
+  });
+};
+
 // app.get('/api/v1/tours', getAllTour);
 // app.get('/api/v1/tours/:id', getTour);
 // app.post('/api/v1/tours', postTour);
@@ -126,6 +161,17 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/api/v1/user/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // 4) Start Server
 const port = 3000;
