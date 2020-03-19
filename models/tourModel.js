4f376d62b88f3d133a0cf3eb212d2c48eaa12d54
Ade Rizky <aderizkyfaqi@gmail.com>
2020-03-20 06:13:25 +0700

    Modeling tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tours must have name!'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a dutarion']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tours must have price!']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cove image']
+  },
+  images: [String],
+  creatAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 //model
