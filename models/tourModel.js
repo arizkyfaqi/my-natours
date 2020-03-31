57b718a84953ac1f8d71198163014fe229ec8e6d
arfa <arfa@example.com>
2020-04-01 03:12:25 +0700

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 //Schema
 const tourSchema = new mongoose.Schema(
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a dutarion']
@@ -63,6 +65,22 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeek').get(function() {
   return this.duration / 7;
+});
+
+//DOCUMENT MIDDLEWARE: run before .save() and create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', function(next) {
+  console.log('Will save document');
+  next();
+});
+
+tourSchema.post('save', function(doc, next) {
+  console.log(doc);
+  next();
 });
 
 //model
