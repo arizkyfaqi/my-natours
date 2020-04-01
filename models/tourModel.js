43bbdc767a0a5c6a64c2e9d1e21953e2e775d8d2
arfa <arfa@example.com>
2020-04-01 21:26:15 +0700

    query  middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,7 +55,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: true
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -67,22 +71,37 @@ tourSchema.virtual('durationWeek').get(function() {
   return this.duration / 7;
 });
 
-//DOCUMENT MIDDLEWARE: run before .save() and create()
+//DOCUMENT MIDDLEWARE: run before .save() and create() | from mongoose
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-tourSchema.pre('save', function(next) {
-  console.log('Will save document');
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+//QUERY MIDDLEWARE | from MongoDB
+// tourSchema.pre('find', function(next) {
+// using regular expresion execute find(), findOne(), and another else with find
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
   next();
 });
 
-tourSchema.post('save', function(doc, next) {
-  console.log(doc);
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Our QUERY took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
   next();
 });
-
 //model
 const Tour = mongoose.model('Tours', tourSchema);
 module.exports = Tour;
