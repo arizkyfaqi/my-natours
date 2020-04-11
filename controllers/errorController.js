099e639fdc92d13014e6b3aabbda7c8d55ef8d75
arfa <arfa@example.com>
2020-04-12 06:16:13 +0700

    error during dev vs prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,33 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Oprational, trusted error: send message to client
+  if (err.isOprational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    //Programming or other unknow error: don't lak error details
+  } else {
+    //1) Log error
+    console.error('ERROR : ', err);
+
+    //2) Send generit message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //log the error
   // console.log(err.stack);
@@ -5,8 +35,9 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
