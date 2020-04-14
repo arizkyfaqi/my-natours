5cd4c9ae869fbac7c3bf53f7fbe296ffef55ae6f
arfa <arfa@example.com>
2020-04-15 03:50:07 +0700

    Managing passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //name, email, photo, password, passwordChonfirm
 const userSchema = new mongoose.Schema({
@@ -9,7 +11,8 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
-    required: [true, 'Please provide your email!']
+    required: [true, 'Please provide your email!'],
+    validate: [validator.isEmail, 'Please provide a valid email']
   },
   photo: String,
   password: {
@@ -19,8 +22,28 @@ const userSchema = new mongoose.Schema({
   },
   passwordChonfirm: {
     type: String,
-    require: [true, 'Please confrim your password!']
+    require: [true, 'Please confrim your password!'],
+    validate: {
+      //This only works on CREATE and SAVE
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
   }
+});
+
+//Hashing password
+userSchema.pre('save', async function(next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete passwordConfrim
+  this.passwordChonfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
