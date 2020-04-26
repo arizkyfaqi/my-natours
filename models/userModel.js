3b9923121a30dd547027e0156a001e0553a5aa14
arfa <arfa@example.com>
2020-04-26 21:36:33 +0700

    deleting the current user

--- models/userModel.js
+++ models/userModel.js
@@ -40,7 +40,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangeAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 //Hashing password
@@ -60,6 +65,12 @@ userSchema.pre('save', function(next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangeAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function(next) {
+  //this points to the current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
