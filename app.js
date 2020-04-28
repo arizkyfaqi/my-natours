72562257349eb55f94051cf4dce603f8b3bdc095
arfa <arfa@example.com>
2020-04-28 23:15:54 +0700

    rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
@@ -8,12 +9,18 @@ const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-// 1) Middleware
+// 1) Global Middleware
 // 3rd-party Middleware
-// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 //access static files
